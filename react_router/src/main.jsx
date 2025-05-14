a1960e1dffed9bfe695669b407f5d89c2daefc41
Rajs1236 <raj22f069@gmail.com>
2025-05-14 06:27:15 +0000

    cmt

--- react_router/src/main.jsx
+++ react_router/src/main.jsx
@@ -3,32 +3,55 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Layout from './Layout.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github,{githubinfoloader} from './components/GITHUB/Github.jsx'
+// const router=createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       },
+//       {
+//         path:"user/:userid",
+//         element:<User/>
+//       }, {
+//         path:"github",
+//         element:<Github/>
+//       }
+//     ]
+//   }
+// ])
+const router=createBrowserRouter(
+createRoutesFromElements(
+  <Route path='/'element={<Layout/>}>
+    <Route path=''element={<Home/>}/>
+    <Route path='about'element={<About/>}/>
+     <Route path='contact'element={<Contact/>}/>
+      <Route path='user/:userid'element={<User/>}/>
+       <Route path='github'element={<Github/>}/>
+       <Route loader={githubinfoloader}
+       path='github'
+       element={<Github/>}
+       />
+  </Route>
+))
 
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout/>,
-    children:[
-      {path:"",
-        element:<Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      },
-      {
-        path:"contact",
-        element:<Contact/>
-      }
-    ]
-  }
-])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <RouterProvider router={router}/>
-  </StrictMode>,
+  </StrictMode>
 )
